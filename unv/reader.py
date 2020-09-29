5a7e0e5c46c2caa9dc83325983f53d5ea01eb83b
Dionisis Pettas <dspedi@gmail.com>
2020-09-29 19:04:29 +0300

    exportTofolder stores the boundary elements

--- unv/reader.py
+++ unv/reader.py
@@ -1,6 +1,8 @@
 from   unv.group   import *
 from   unv.session import *
-import numpy      as np
+
+import numpy       as np
+import os 
 
 
 DELIM = "-1"
@@ -13,30 +15,27 @@ def isDelimiter(line: str):
 #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 class Reader():
+    """
+    """
+
     def __init__(self, filename):
-        """
-        ** The Reader Class reads a file with a  specific filename.
-        ** The file contains:
-            filename : the name of the file
-            nodes    : the coordinates of the nodes
-            surface_elements : the boundary elements of the domain
-            elements         : the elements of the domain
-        """
-        self.filename         = filename
-        self.nodes            = np.ndarray(0)
-        self.surface_elements = np.ndarray(0)
+
+        self.filename         = filename 
+        self.nodes            = np.ndarray(0) 
+        self.surface_elements = np.ndarray(0) 
         self.elements         = np.ndarray(0)
+       
 
 
         self.__file     = open(self.filename)
-        self.__lines    = self.__file.readlines()
-
+        self.__lines    = self.__file.readlines() 
+        
         self.__sessions = []
         self.__groups   = []
-
+        
         self.__read_structure()
         self.__UNV_id_2411()
-        self.__UNV_id_2412()
+        self.__UNV_id_2412() 
         self.__UNV_id_2467()
 
 
@@ -60,7 +59,7 @@ class Reader():
         raise ValueError("""the name of the group does not exist.
                             Possible values {}""".format(names) )
 
-    def getNumNodes(self):
+    def getNumNodes(self): 
         return self.nodes.shape[0]
 
     def getNumElements(self):
@@ -69,40 +68,40 @@ class Reader():
     def getNumSurfaceElements(self):
         return self.surface_elements.size
 
+    def getSurfaceElementsThatBelongToGroup(self, groupname: str):
+        return np.asarray(self.getGroup(groupname).connectivity, dtype = int)
 
     def getNodeIDsThatBelongToGroup(self,groupname: str):
-
-        out = []
+        
+        out = [] 
         for elementid in self.getGroup(groupname).connectivity:
+            
             element = self.surface_elements[elementid]
+            
             out.append( element[0] )
-            out.append( element[1] )
 
-        out = list( dict.fromkeys(out) )
-
-        return np.asarray( out ) 
+        out.append( element[1] )
+        return out 
 
     def getNodeCoordinateThatBelongToGroup(self, groupname: str,  direction: str):
-
+        
         ids = self.getNodeIDsThatBelongToGroup(groupname)
 
-        if   direction.lower() == 'x': dire_ = 0
-        elif direction.lower() == 'y': dire_ = 1
-        elif direction.lower() == 'z': dire_ = 2
-        else: raise ValueError("Wrong Value for direction. Possible: x,y,z")
+        if   direction.lower() == 'x'  : return np.asarray([self.nodes[i][0] for i in ids] )
+        elif direction.lower() == 'y'  : return np.asarray([self.nodes[i][1] for i in ids] )
+        elif direction.lower() == 'z'  : return np.asarray([self.nodes[i][2] for i in ids] )
+        elif direction.lower() == 'all': return np.asarray([self.nodes[i]    for i in ids] )
+        else: raise ValueError("Wrong Value for direction. Possible: x,y,z or all")
 
-        out = []
-        out = [self.nodes[i][dire_] for i in ids]
-
-        return np.asarray( out )
-
+        return np.asarray([self.nodes[i][dire_] for i in ids] )
+            
     def getNodeCoordinates(self, direction: str):
 
         if   direction.lower() == 'x': dire_ = 0
         elif direction.lower() == 'y': dire_ = 1
         elif direction.lower() == 'z': dire_ = 2
         else: raise ValueError("Wrong Value for direction. Possible: x,y,z")
-
+ 
         out = np.ndarray( self.getNumNodes() )
         for i in range(self.getNumNodes() ):
             out[i] = self.nodes[i][dire_]
@@ -111,20 +110,36 @@ class Reader():
     def getElements(self):
         return self.elements
 
+    def exportTofolder(self, foldername: str, zero_based: bool = True):
+       
+        if not os.path.exists(foldername):
+            os.mkdir(foldername) 
 
-    def close(self):
 
-        self.__file.close()
-        self.filename         = None
-        self.nodes            = None
-        self.surface_elements = None
-        self.elements         = None
+        if not zero_based: base = 1
+        else             : base = 0
 
-        self.__file           = None
-        self.__lines          = None
 
-        self.__sessions       = None
-        self.__groups         = None
+        saveToFile = lambda filename, array, dtype:np.savetxt(os.path.join(foldername,filename) ,
+                                                             array,fmt=dtype,
+                                                             delimiter='\t',
+                                                             header = "{}".format( array.shape[0] ) )
+
+        saveToFile( "nodes.dat"           , self.nodes           , '%.18f')# Write Nodes
+        saveToFile( "elements.dat"        , self.elements+base   , '%d'   )# Write Elements
+        saveToFile( "surface_elements.dat", self.surface_elements+base, '%d'   )# Surface Elements
+        #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        # Write Boundaries 
+        #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+        ibnd = 0
+        for bnd in self.getGroupNames():
+            ibnd += 1
+            
+            bndname   = "bnd_{}_{}_elements.dat".format( ibnd, bnd)
+            selements = self.getSurfaceElementsThatBelongToGroup(bnd)
+            saveToFile(bndname, selements + base,'%d')
+
+
 
     #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
     # Private Methods  
@@ -132,20 +147,20 @@ class Reader():
 
     def __read_structure(self):
 
-        opened_session = False
-        for i,line in enumerate(self.__lines):
-
-            if   isDelimiter(line) and not opened_session:
+        opened_session = False 
+        for i,line in enumerate(self.__lines): 
+            
+            if   isDelimiter(line) and not opened_session: 
 
                 opened_session = True
-                init_session   = i + 1
+                init_session   = i + 1            
 
-            elif isDelimiter(line) and     opened_session:
+            elif isDelimiter(line) and     opened_session: 
 
                 opened_session = False
                 final_session  = i - 1
 
-                session_lines = self.__lines[init_session:final_session+1]
+                session_lines = self.__lines[init_session:final_session+1] 
 
                 self.__sessions.append( UnvSession(session_lines) )
 
@@ -157,15 +172,15 @@ class Reader():
         node = []
         i = 0
         for line in self.getSessionWithId(2411):
-            i +=1
+            i +=1            
             if not i%2 == 0 and i != 1: node.append( [float(_) for _ in line.split()] )
 
 
 
-        self.nodes = np.asarray ( node )
+        self.nodes = np.asarray ( node ) 
 
     def __UNV_id_2412(self):
-
+        
         OneDimensionalElementsFlag = 11
 
         OneDimensionalElement      = []
@@ -183,7 +198,7 @@ class Reader():
             if elementType == 11 and numNodes == 2:
                 i += 2
                 OneDimensionalElement.append( [int(_)-1 for _ in line[i].split()])
-            if elementType == 41 and numNodes == 3:
+            if elementType == 41 and numNodes == 3:                 
                 i += 1
                 TwoDimensionalElement.append( [int(_)-1 for _ in line[i].split()])
 
@@ -202,21 +217,18 @@ class Reader():
 
         # First Element of the the session is the gid of the session
         i = 1
-        while True:
+        while True: 
 
             init   = i
             groupLines  = [int(_) for _ in line[i].split()] [-1]
-            groupLines  = groupLines + groupLines%2
+            groupLines  = groupLines + groupLines%2   
             groupLines  = int(groupLines/2 )
             i +=  groupLines + 1 # plus one line which is the title
             final = i
 
-            if ( groupLines > 0) :
+            if ( groupLines > 0) : 
 
-                self.__groups.append ( Group(  line[init:final+1] ) )
-
-
-
+                self.__groups.append ( Group(  line[init:final+1] ) ) 
 
             if i >= len(line) -1: break
-
+    
