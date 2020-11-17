32be9cc821eab4c5fd93568501b7a94dbf505d77
Dionisis Pettas <dspedi@gmail.com>
2020-11-17 18:09:32 +0200

    Fixing example

--- test.py
+++ test.py
@@ -3,55 +3,62 @@ import unv
 import timeit 
 
 
+# initial   = unv.Reader("/home/pettas/MyWork/LabFem/UnBounded.unv")
+# xt        = initial.getNodeCoordinates('x')
+# yt        = initial.getNodeCoordinates('y')
+# triangles = initial.getElements()
+
+# plt.triplot(xt, yt, triangles, linewidth = 1.5)
 # Read the unv file
-unvfile = unv.Reader("./Mesh_2.unv")
-
-
+refinement = unv.Reader("./Mesh_2.unv")
 
 # Get the coordinates of the bulk nodes 
-xt = unvfile.getNodeCoordinates('x')
-yt = unvfile.getNodeCoordinates('y')
+xt        = refinement.getNodeCoordinates('x')
+yt        = refinement.getNodeCoordinates('y')
+triangles = refinement.getElements()
+
+
+plt.triplot(xt, yt, triangles, linewidth = 1.5)
+
+
+plt.axis('off')
+plt.axes().set_aspect(1.0)
+plt.tight_layout()
+
+plt.show()
 
 # Get the bulk elements
-triangles = unvfile.getElements()
 
 
 # Boundary elements 
 # Read Groups and plot the boundary nodes 
 
-color = "#000000"
-x = unvfile.getNodeCoordinateThatBelongToGroup('Inflow','x')
-y = unvfile.getNodeCoordinateThatBelongToGroup('Inflow','y')
-for i in range(x.size): plt.scatter(x,y,color = color, s = 10)
+#color = "#000000"
+#x = unvfile.getNodeCoordinateThatBelongToGroup('Inflow','x')
+#y = unvfile.getNodeCoordinateThatBelongToGroup('Inflow','y')
+#for i in range(x.size): plt.scatter(x,y,color = color, s = 10)
 
-x = unvfile.getNodeCoordinateThatBelongToGroup('wall','x')
-y = unvfile.getNodeCoordinateThatBelongToGroup('wall','y')
-for i in range(x.size): plt.scatter(x,y,color = color, s = 10)
+# x = unvfile.getNodeCoordinateThatBelongToGroup('Wall','x')
+# y = unvfile.getNodeCoordinateThatBelongToGroup('Wall','y')
+# for i in range(x.size): plt.scatter(x,y,color = color, s = 10)
 
-x = unvfile.getNodeCoordinateThatBelongToGroup('Outflow','x')
-y = unvfile.getNodeCoordinateThatBelongToGroup('Outflow','y')
-for i in range(x.size): plt.scatter(x,y,color = color, s = 10)
+# x = unvfile.getNodeCoordinateThatBelongToGroup('Outflow','x')
+# y = unvfile.getNodeCoordinateThatBelongToGroup('Outflow','y')
+# for i in range(x.size): plt.scatter(x,y,color = color, s = 10)
 
-x = unvfile.getNodeCoordinateThatBelongToGroup('Symmetry1','x')
-y = unvfile.getNodeCoordinateThatBelongToGroup('Symmetry1','y')
-for i in range(x.size): plt.scatter(x,y,color = color, s = 10)
+#x = unvfile.getNodeCoordinateThatBelongToGroup('Symmetry1','x')
+#y = unvfile.getNodeCoordinateThatBelongToGroup('Symmetry1','y')
+#for i in range(x.size): plt.scatter(x,y,color = color, s = 10)
 
-x = unvfile.getNodeCoordinateThatBelongToGroup('Bubble','x')
-y = unvfile.getNodeCoordinateThatBelongToGroup('Bubble','y')
-for i in range(x.size): plt.scatter(x,y,color = color, s = 10)
+# x = unvfile.getNodeCoordinateThatBelongToGroup('Bubble','x')
+# y = unvfile.getNodeCoordinateThatBelongToGroup('Bubble','y')
+# for i in range(x.size): plt.scatter(x,y,color = color, s = 10)
 
-x = unvfile.getNodeCoordinateThatBelongToGroup('Symmetry2','x')
-y = unvfile.getNodeCoordinateThatBelongToGroup('Symmetry2','y')
-for i in range(x.size): plt.scatter(x,y,color = color, s = 10)
+#x = unvfile.getNodeCoordinateThatBelongToGroup('Symmetry2','x')
+#y = unvfile.getNodeCoordinateThatBelongToGroup('Symmetry2','y')
+#for i in range(x.size): plt.scatter(x,y,color = color, s = 10)
 
 
-
-plt.axis('off')
-plt.triplot(xt, yt, triangles, linewidth = 0.5)
-plt.axes().set_aspect(1.0)
-plt.tight_layout()
-
-plt.show()
 
 
 
